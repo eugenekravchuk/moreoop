88906ee908625a8f12000714b53187e2e8096f92
eugenekravchuk <jenia.kravchuk223@gmail.com>
2024-09-29 19:39:21 +0300

    fixed noble

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -6,12 +6,16 @@ import lotr.Character;
 import lotr.kickstrategy.NobleKick;
 
 public abstract class Noble extends Character {
+  private static final Random RANDOM = new Random();
+
   public Noble(int min, int max) {
-      super(new Random().nextInt(min, max), new Random().nextInt(min, max), new NobleKick());
+    // Adjusted to use nextInt with a calculated range
+    super(min + RANDOM.nextInt(max - min), min + RANDOM.nextInt(max - min), new NobleKick());
   }
   
   @Override
   public void kick(Character c) { 
-    c.setHp(c.getHp() - new Random().nextInt(0, c.getPower()));
+    // Adjusted to use nextInt with a calculated range
+    c.setHp(c.getHp() - RANDOM.nextInt(c.getPower() + 1)); // +1 to include the upper bound
   }
 }
