0cf8a030e76d2ff258e357916f46d1a4a66509cd
eugenekravchuk <jenia.kravchuk223@gmail.com>
2024-09-29 19:37:11 +0300

    fixed character factory

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -3,19 +3,21 @@ package lotr;
 import java.util.Random;
 
 public class CharacterFactory {
-  public static lotr.Character createCharacter() { 
+  public static lotr.Character createCharacter() {
     Class<? extends lotr.Character>[] characters = new Class[] {
       Elf.class,
       Hobbit.class,
       King.class,
       Knight.class
     };
-    int randomIndex = new Random().nextInt(characters.length);
-    try {
 
+    Random random = new Random();
+    int randomIndex = random.nextInt(characters.length);
+
+    try {
       return characters[randomIndex].getDeclaredConstructor().newInstance();
     } catch (Exception e) {
       throw new RuntimeException("Character creation failed", e);
     }
-  } 
+  }
 }
